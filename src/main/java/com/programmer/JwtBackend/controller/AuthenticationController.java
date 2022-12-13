4e46bff4e0bcbc7a70d89af62f12a22b80883b0f
lukasz <lukaszsikorski19933@gmail.com>
2022-12-13 16:01:11 +0100

    Change type of result

--- src/main/java/com/programmer/JwtBackend/controller/AuthenticationController.java
+++ src/main/java/com/programmer/JwtBackend/controller/AuthenticationController.java
@@ -1,8 +1,12 @@
 package com.programmer.JwtBackend.controller;
 
 import com.programmer.JwtBackend.controller.dto.request.JwtAuthenticationRequest;
+import com.programmer.JwtBackend.controller.dto.request.UserRequest;
 import com.programmer.JwtBackend.controller.dto.response.JwtResponse;
+import com.programmer.JwtBackend.controller.dto.response.MessageResponse;
+import com.programmer.JwtBackend.domain.AppUser;
 import com.programmer.JwtBackend.service.AuthService;
+import com.programmer.JwtBackend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -18,12 +22,15 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class AuthenticationController {
 
     private final AuthService authService;
-
+    private final UserService userService;
     @PostMapping(value = "/login")
-    public ResponseEntity<JwtResponse> login(@RequestBody JwtAuthenticationRequest authRequest) throws Exception {
+    public ResponseEntity<JwtResponse> loginUser(@RequestBody JwtAuthenticationRequest authRequest) throws Exception {
         return ResponseEntity.ok(new JwtResponse(authService.createJwt(authRequest)));
     }
 
-
+    @PostMapping(value = "/register")
+    public ResponseEntity<AppUser> registerUser(@RequestBody UserRequest request){
+        return ResponseEntity.ok(authService.registerUser(request));
+    }
 
 }
