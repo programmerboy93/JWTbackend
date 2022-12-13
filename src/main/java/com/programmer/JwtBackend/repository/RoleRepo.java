426c81b1ca593aa40206b6f77917836f2b5f7d81
lukasz <lukaszsikorski19933@gmail.com>
2022-12-13 15:57:20 +0100

    Change implemented findByName method

--- src/main/java/com/programmer/JwtBackend/repository/RoleRepo.java
+++ src/main/java/com/programmer/JwtBackend/repository/RoleRepo.java
@@ -1,8 +1,11 @@
 package com.programmer.JwtBackend.repository;
 
+import com.programmer.JwtBackend.domain.ERole;
 import com.programmer.JwtBackend.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepo extends JpaRepository<Role, Long> {
-    Role findByName(String name);
+    Optional<Role> findByName(ERole name);
 }
