d4f6fbabe33fea359b792fb3c75be516590f9d93
lukasz <lukaszsikorski19933@gmail.com>
2022-12-13 15:57:56 +0100

    Add existsByEmail method

--- src/main/java/com/programmer/JwtBackend/repository/UserRepo.java
+++ src/main/java/com/programmer/JwtBackend/repository/UserRepo.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
+
+    boolean existsByEmail(String username);
 }
