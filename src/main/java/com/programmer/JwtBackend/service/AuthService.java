ae8ae4c2f435823c79fb2bcaba3a1249485e6fc3
lukasz <lukaszsikorski19933@gmail.com>
2022-12-13 16:01:42 +0100

    Implement register methods

--- src/main/java/com/programmer/JwtBackend/service/AuthService.java
+++ src/main/java/com/programmer/JwtBackend/service/AuthService.java
@@ -2,30 +2,45 @@ package com.programmer.JwtBackend.service;
 
 import com.programmer.JwtBackend.config.JwtUtils;
 import com.programmer.JwtBackend.config.MyUserDetails;
+import com.programmer.JwtBackend.config.validator.EmailValidator;
 import com.programmer.JwtBackend.controller.dto.request.JwtAuthenticationRequest;
+import com.programmer.JwtBackend.controller.dto.request.UserRequest;
 import com.programmer.JwtBackend.domain.AppUser;
+import com.programmer.JwtBackend.domain.ERole;
+import com.programmer.JwtBackend.domain.Role;
+import com.programmer.JwtBackend.repository.RoleRepo;
 import com.programmer.JwtBackend.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 @Log4j2
 public class AuthService {
-    private final UserRepo userRepository;
+    private final UserRepo userRepo;
+
+    private final RoleRepo roleRepo;
 
     private final JwtUtils jwtUtils;
 
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final UserService userService;
+
+    private final EmailValidator emailValidator;
 
     private final AuthenticationManager authenticationManager;
+
+    private final PasswordEncoder passwordEncoder;
 
     public String createJwt(JwtAuthenticationRequest request) {
         try {
@@ -37,9 +52,24 @@ public class AuthService {
         } catch (Exception e) {
             log.error(e.getMessage());
         }
-        final AppUser user = userRepository.findByEmail(request.username())
+        final AppUser user = userRepo.findByEmail(request.username())
                 .orElseThrow(() -> new UsernameNotFoundException("User could not be found"));
         final MyUserDetails userDetails = new MyUserDetails(user);
         return jwtUtils.generateJwtToken(userDetails);
+    }
+
+    public AppUser registerUser(UserRequest request) {
+        if (!emailValidator.test(request.username())) {
+            throw new IllegalArgumentException("Not a valid email");
+        } else if (userRepo.existsByEmail(request.username())) {
+            throw new IllegalArgumentException("Email already taken");
+        }
+        return registerHelper(request);
+    }
+
+    private AppUser registerHelper(UserRequest request) {
+        final Role byNameE = roleRepo.findByName(ERole.ROLE_USER).orElseThrow(() -> new RuntimeException("Role is not found"));
+        final AppUser user = new AppUser(null, request.name(), request.username(), passwordEncoder.encode(request.password()), Set.of(byNameE));
+        return userRepo.save(user);
     }
 }
