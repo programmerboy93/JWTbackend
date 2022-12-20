e69712f6b2bd77b6505a6505ce4b3d24af9c9583
lukasz <lukaszsikorski19933@gmail.com>
2022-12-20 16:49:39 +0100

    Add JwtAccessDeniedHandler

---
+++ src/main/java/com/programmer/JwtBackend/auth/exception/JwtAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package com.programmer.JwtBackend.auth.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Log4j2
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        log.error("Forbidden error: {}", accessDeniedException.getMessage());
+
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
+
+    }
+}
