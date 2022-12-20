002aa6afe68e0006ca4a3ddc1bc318e2637cb228
lukasz <lukaszsikorski19933@gmail.com>
2022-12-20 16:50:05 +0100

    Add JavaMailSender bean

---
+++ src/main/java/com/programmer/JwtBackend/email/JavaMailSenderConfiguration.java
@@ -0,0 +1,16 @@
+package com.programmer.JwtBackend.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class JavaMailSenderConfiguration {
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        return new JavaMailSenderImpl();
+    }
+
+}
